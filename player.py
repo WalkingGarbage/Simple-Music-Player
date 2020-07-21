04d279c1d0c94c35b0d3ae4611f434a932f01c21
LNZ <35008288+WalkingGarbage@users.noreply.github.com>
2020-07-21 10:17:52 +0200

    Add files via upload

--- player.py
+++ player.py
@@ -1,40 +1,19 @@
 #!/usr/bin/env python3
 
-import gi
-import multiprocessing
-from playsound import playsound
 import os
-import sys
-import globals
 
-def play(track, path):                                          #plays a song
-    file = path + track
-    p = multiprocessing.Process(target=playsound, args=(file,))
-    p.start()
-    input("press ENTER to stop the track: ")
-    p.terminate()
+class queue:
 
+    def __init__(self, path):
+        self.dir = path
+        pass
 
-def select(list):                                               #select the song to play from the queue
-    while True:
-        i = input("Select the audio (number): ")
-        if i == '':
-            i=0
-        else:
-            i = int(i)
-        if i <= len(list) and i!=0:
-            return (i - 1)
-            break
-        else:
-            print("The number must be in the given list!")
-
-
-def listtracks(path):                                           #prints a list of the songs in a folder and puts them on the queue
-    globals.initialize()
-    x = 0
-    for file in os.listdir(path):
-        if file.endswith(".mp3") or file.endswith(".wav"):
-            print(str(x+1) + ' ' + file)
-            globals.queue = globals.queue + [file]
-            x=x+1
+    def listtracks(self):
+        tracks = []
+        x = 0
+        for file in os.listdir(self.dir):
+            if file.endswith(".mp3") or file.endswith(".wav"):
+                print(str(x+1) + ' ' + file)
+                tracks = tracks + [file]
+                x=x+1
 
