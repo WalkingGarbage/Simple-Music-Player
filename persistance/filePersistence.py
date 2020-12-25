873a6b9f880a30638e3209ee9e6262c88a8c72cf
open-antux <antonio.giustino11@gmail.com>
2020-12-25 22:21:34 +0100

    Created a new persistence

---
+++ persistance/filePersistence.py
@@ -0,0 +1,44 @@
+import os
+
+class Persistence:
+
+    def __init__(self):
+        """ Initializing and creating settings config file and directory """
+
+        self.__path = os.path.expanduser("~") + "/Teddie Music/"
+
+        if not os.path.exists(self.__path):
+            os.makedirs(self.__path)
+        
+        try:
+            os.makedirs(self.__path + "Music")
+            os.mknod(self.__path + "playlist.txt")
+        except:
+            pass
+
+        self.__musicDirectory = self.__path + "Music"
+        self.__playlist = self.__path + "playlist.txt"
+
+    def createPlaylistFile(self):
+        """ Populate of songs the playlist """
+
+        f = open(self.__playlist, "w+")
+
+        for dirpath, _, files in os.walk(self.__musicDirectory):  
+            for song in files:
+                if song.endswith(".mp3") or song.endswith(".wav"):
+                    f.writelines(dirpath + "/" + song + "\n")
+
+        f.close()
+    
+    def getSongN(self, n):
+        """ Return the path of the song on the N line of the file """
+
+        f = open(self.__playlist, "r")
+        songs = f.readlines()
+
+        if(n > len(songs)):
+            n = 0
+
+        return songs[n].replace("\n", "")
+
